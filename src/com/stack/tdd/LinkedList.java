c12bf146469f4801b61e26a474456f64f81e3092
luckycyber <chika.onyima@gmail.com>
2020-03-07 00:13:24 -0500

    The tests currently only cover adding the first element beyonf that it will likely fail.

--- src/com/stack/tdd/LinkedList.java
+++ src/com/stack/tdd/LinkedList.java
@@ -1,9 +1,24 @@
 package com.stack.tdd;
 
-public class LinkedList {
+public class LinkedList<T> {
+	
+	private int size = 0;
+	private Node first;
 	
 	public int size() {
-		return 0;
+		return size;
+	}
+	
+	public void add(T value) {
+		size++;
+		first = new Node(value);
+		
+	}
+	private class Node{
+		private final T value;
+		public Node(T value) {
+			this.value = value;
+		}
 	}
 
 }
--- src/com/stack/tdd/LinkedListTest.java
+++ src/com/stack/tdd/LinkedListTest.java
@@ -3,15 +3,28 @@ package com.stack.tdd;
 import java.util.LinkedList;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LinkedListTest {
 	
+	private LinkedList<String> list;
+	
+	@Before
+	public void setUp() {
+		list = new LinkedList<>();
+	}
 	@Test
 //	public void testingInitialListIsEmpty() {
 	public void testingSize_initialList() {
 		LinkedList list = new LinkedList();
 		Assert.assertEquals(0, list.size());
+	}
+	
+	@Test
+	public void testSize_oneElement_sizeIsOne() {
+		list.add("a");
+		Assert.assertEquals(1,list.size());
 	}
 
 }
